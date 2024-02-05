e2f10f86c01e22cd8cd1e60932b94ca05337f57a
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-02-05 09:49:46 +0100

    create redeem script and derive address

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,4 +1,4 @@
-import { Transaction, script } from 'bitcoinjs-lib';
+import { Network, Transaction, crypto, payments, script } from 'bitcoinjs-lib';
 
 export class TransactionService {
     public static decodeTransactionHex(hex: string) {
@@ -41,5 +41,42 @@ export class TransactionService {
 
         // Convert the script to ASM
         return script.toASM(parsedScript);
+    }
+
+    public static createRedeemScript(preimage: string) {
+        // Calculate SHA256 hash of the preimage
+        const sha256Hash = crypto.sha256(Buffer.from(preimage, 'utf8')).toString('hex');
+
+        // Create the redeem script for the given script  OP_SHA256 <lock_hex> OP_EQUAL
+        const redeemScript = script.compile([
+            script.OPS.OP_SHA256,
+            Buffer.from(sha256Hash, 'hex'),
+            script.OPS.OP_EQUAL,
+        ]);
+
+        // Convert redeem script to hex
+        return redeemScript.toString('hex');
+    }
+
+    public static deriveP2WSHAddress(redeemScriptHex: string, appNetwork: Network): string {
+        // Create a P2WSH (Pay-to-Witness-Script-Hash) address from the redeem script
+        const scriptBuffer = Buffer.from(redeemScriptHex, 'hex');
+        const p2wsh = payments.p2wsh({
+            redeem: { output: scriptBuffer, network: appNetwork },
+        });
+        return p2wsh.address as string;
+    }
+
+    public static async getUnspentBitcoinOutputs(address: string) {
+        // Use a blockchain explorer API to get unspent outputs (UTXOs) for the given address
+        const url = `https://blockstream.info/api/address/${address}/utxo`;
+
+        try {
+            const response = await fetch(url);
+            const utxos = await response.json();
+            return utxos;
+        } catch (error) {
+            throw new Error(`Unable to get UTXOs for address: ${address}`);
+        }
     }
 }
