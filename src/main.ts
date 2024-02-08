f2160882576d82e86074b953e674bc9f4d545f70
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-02-08 09:50:10 +0100

    solution for 3 and 4

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { handleGenerateAddress } from './handlers/gen.address.handler';
 import { networkSelectorHandler } from './handlers/network.selector.handler';
 import { handleTransactions } from './handlers/transactions.handler';
 
+import { TransactionService } from './services/transaction.service';
+
 // Define CLI commands
 program
     .command('start')
@@ -13,6 +15,25 @@ program
     .action(async () => {
         const user = await handleAuth();
         const { appNetwork } = await networkSelectorHandler();
+
+        /**
+         * Below is an example of how to use the TransactionService to create a signed transaction
+         * you can use the credentials test:test123456789012 to run this example
+         */
+        const addressWithBalance = '2MwrinsNPrqwqup5G5DQ5BG2XnxjLs8y28z';
+        const redeemScriptHex = TransactionService.createRedeemScript('Btrust Builders');
+        const p2wshAddress = TransactionService.deriveP2WSHAddress(redeemScriptHex, appNetwork);
+        console.log(`P2WSH Address: ${p2wshAddress}`);
+        const transaction = await TransactionService.createSignedTransaction(
+            addressWithBalance,
+            p2wshAddress,
+            user.seed as string,
+            appNetwork,
+            redeemScriptHex,
+            82312,
+        );
+        console.log(transaction);
+
         await handleCheckBalance();
         await handleTransactions();
         await handleGenerateAddress(user, appNetwork);
--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -1,4 +1,7 @@
-import { Network, Transaction, crypto, payments, script } from 'bitcoinjs-lib';
+import BIP32Factory from 'bip32';
+import { Network, Psbt, Transaction, crypto, networks, payments, script } from 'bitcoinjs-lib';
+import * as ecc from 'tiny-secp256k1';
+import { TransactionResponse } from '../types/interface';
 
 export class TransactionService {
     public static decodeTransactionHex(hex: string) {
@@ -48,14 +51,13 @@ export class TransactionService {
         const sha256Hash = crypto.sha256(Buffer.from(preimage, 'utf8')).toString('hex');
 
         // Create the redeem script for the given script  OP_SHA256 <lock_hex> OP_EQUAL
-        const redeemScript = script.compile([
+        const compiledScript = script.compile([
             script.OPS.OP_SHA256,
             Buffer.from(sha256Hash, 'hex'),
             script.OPS.OP_EQUAL,
         ]);
 
-        // Convert redeem script to hex
-        return redeemScript.toString('hex');
+        return compiledScript.toString('hex');
     }
 
     public static deriveP2WSHAddress(redeemScriptHex: string, appNetwork: Network): string {
@@ -67,14 +69,78 @@ export class TransactionService {
         return p2wsh.address as string;
     }
 
-    public static async getUnspentBitcoinOutputs(address: string) {
+    public static async createSignedTransaction(
+        fromAddress: string,
+        toAddress: string,
+        seed: string,
+        appNetwork: Network,
+        redeemScriptHex: string,
+        amount: number,
+    ) {
+        // Create a new Psbt instance
+        const psbt = new Psbt({ network: appNetwork });
+
+        // Create a BIP32 instance from the seed
+        const seedBuffer = Buffer.from(seed, 'hex');
+        const bip32 = BIP32Factory(ecc);
+        const rootKey = bip32.fromSeed(seedBuffer, appNetwork);
+
+        // Derive the key pair from the root key
+        const keyPair = rootKey.derivePath(`m/44'/0'/0'/0/0'`);
+
+        // Get unspent outputs for the from address
+        const utxos = await this.getUnspentBitcoinOutputs(fromAddress, appNetwork);
+
+        // Calculate the total value of the unspent outputs
+        const totalValue = utxos.reduce((prev, curr) => prev + curr.value, 0);
+        console.log('Total value of unspent outputs:', totalValue);
+
+        // Add the inputs to the Psbt instance
+        utxos.forEach(utxo => {
+            const transactionHash = Buffer.from(utxo.txid, 'hex').reverse();
+            const redeemScript = Buffer.from(
+                'a91432971b01a505e62b860ed9c960f98b64252b294287',
+                'hex',
+            );
+            psbt.addInput({
+                hash: utxo.txid,
+                index: 0,
+                witnessUtxo: {
+                    script: Buffer.from('a91432971b01a505e62b860ed9c960f98b64252b294287', 'hex'),
+                    value: 113593,
+                },
+                nonWitnessUtxo: transactionHash,
+                redeemScript,
+            });
+        });
+
+        // Add the output to the Psbt instance
+        psbt.addOutput({
+            address: toAddress,
+            value: amount,
+            redeemScript: Buffer.from(redeemScriptHex, 'hex'),
+        });
+
+        // Sign the inputs
+        psbt.signAllInputs(keyPair);
+
+        // Finalize the Psbt instance
+        psbt.finalizeAllInputs();
+
+        // Return the signed transaction as hex
+        return psbt.extractTransaction().toHex();
+    }
+
+    public static async getUnspentBitcoinOutputs(address: string, appNetwork: Network) {
+        const urlRoute = appNetwork === networks.testnet ? '/testnet' : '';
         // Use a blockchain explorer API to get unspent outputs (UTXOs) for the given address
-        const url = `https://blockstream.info/api/address/${address}/utxo`;
+        const url = `https://blockstream.info${urlRoute}/api/address/${address}/utxo`;
+        console.log('URL:', url);
 
         try {
             const response = await fetch(url);
             const utxos = await response.json();
-            return utxos;
+            return utxos as TransactionResponse[];
         } catch (error) {
             throw new Error(`Unable to get UTXOs for address: ${address}`);
         }
